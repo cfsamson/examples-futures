4ad552580e2765bd51323eee0e36f3c01824e6a5
Carl Fredrik Samson <cf@samson.no>
2020-01-23 00:51:28 +0100

    test1

--- src/main.rs
+++ src/main.rs
@@ -25,20 +25,24 @@ fn main() {
     };
 
     let mainfut = async {
-        fut1.await;
-        fut2.await;
+        let handle1 = spawn(fut1);
+        let handle2 = spawn(fut2);
+        handle1.await;
+        handle2.await;
     };
 
-    task::block_on(mainfut);
+    block_on(mainfut);
     reactor.lock().map(|mut r| r.close()).unwrap();
 }
 
 //// ===== EXECUTOR =====
 fn block_on<F: Future>(mut future: F) -> F::Output {
-    let waker = waker_new(thread::current());
+    println!("here");
+    let waker = waker_new(thread::current(), true);
     let waker = waker_into_waker(&waker);
     let mut cx = Context::from_waker(&waker);
     let val = loop {
+        
         let pinned = unsafe { Pin::new_unchecked(&mut future) };
         match Future::poll(pinned, &mut cx) {
             Poll::Ready(val) => break val,
@@ -48,10 +52,46 @@ fn block_on<F: Future>(mut future: F) -> F::Output {
     val
 }
 
+// Poll it once, set the process in motion. The waker we pass in, can instead of
+// waking the thread
+
+// it can return a handle which parks the thread next time it's polled
+
+// The next waker doesn't do anything but waits for the reactor to get a ready
+// value or returns immidiately!
+
+fn spawn<F: Future>(future: F) -> Pin<Box<F>> {
+    let waker = waker_new(thread::current(), false); // new waker, doesn't wake thread
+    let waker = waker_into_waker(&waker); // same
+    let mut cx = Context::from_waker(&waker); // same
+        // it's ok, we don't hold on to anything here
+        let mut boxed = Box::pin(future);
+        //let pinned = unsafe { Pin::new_unchecked(&mut future) };
+        Future::poll(boxed.as_mut(), &mut cx); // we just start the process
+        //let handle = Handle(future);
+        //returns a handle
+        boxed
+    // Handle can be polled blocking the next time
+}
+
+// struct Handle<T: Future >(T);
+// impl<T: Future> Future for Handle<T> 
+// {
+//     type Output = T::Output;
+//     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+//        let pinned = unsafe { Pin::new_unchecked(&mut self) };
+//         match Future::poll(pinned, &mut cx) {
+//             Poll::Pending => Poll::Pending,
+//             Poll::Ready(val) => Poll::Ready(val),
+//         }
+//     }
+// }
+
 // ===== FUTURE IMPLEMENTATION =====
 #[derive(Clone)]
 struct MyWaker {
     thread: thread::Thread,
+    wakeup: bool,
 }
 
 #[derive(Clone)]
@@ -62,12 +102,14 @@ pub struct Task {
     is_registered: bool,
 }
 
-fn waker_new(thread: thread::Thread) -> MyWaker {
-    MyWaker { thread }
+fn waker_new(thread: thread::Thread, wakeup: bool) -> MyWaker {
+    MyWaker { thread, wakeup }
 }
 
 fn waker_wake(s: &MyWaker) {
-    s.thread.unpark();
+    if s.wakeup {
+        s.thread.unpark();
+    }
 }
 
 fn waker_clone(s: &MyWaker) -> RawWaker {
