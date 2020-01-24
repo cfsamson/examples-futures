ba387bb0f9ac782b50d51fa779119bec271449ee
Carl Fredrik Samson <cf@samson.no>
2020-01-24 03:03:48 +0100

    made a working trait object from raw parts

--- src/main.rs
+++ src/main.rs
@@ -1,221 +1,67 @@
-use std::sync::{
-    mpsc::{channel, Sender},
-    Arc, Mutex,
-};
-use std::task::{RawWaker, RawWakerVTable, Waker, Context, Poll};
-use std::thread::{self, JoinHandle};
-use std::time::Duration;
-use std::future::Future;
-use std::pin::Pin;
-use tokio::runtime::Runtime;
-
 fn main() {
-    let reactor = Reactor::new();
-    let reactor = Arc::new(Mutex::new(reactor));
-    let future1 = Task::new(reactor.clone(), 4);
-    let future2 = Task::new(reactor.clone(), 5);
-    //let futures = vec![future1, future2];
+    let mut data = Data::new(2, 3);
+    let vtable = vec![
+        0,            // pointer to `Drop`
+        6,            // Lenght of vtable
+        8,            // alignment
+        add as usize, // function
+        sub as usize, // function
+        mul as usize, // function
+    ];
 
-    let fut1 = async {
-        println!("Tokio got: {}", future1.await);  
-    };
+    let fat_pointer = FatPointer::new(&mut data, vtable.as_ptr());
 
-    let fut2 = async {
-        println!("Tokio got: {}", future2.await);
-    };
+    // we coherce the reference to FatPointer as a pointer
+    let fat_pointer_ptr: *const FatPointer = &fat_pointer;
+    // we then cast the pointer to our FatPointer as a pointer to a pointer to a Trait object
+    // remember that FatPointer is the pointer to our trait object
+    let trait_ptr: *const *const dyn Test = fat_pointer_ptr as *const *const dyn Test;
+    // dereference the traot object (and take a reference to it instead)
+    let test: &dyn Test = unsafe { &**trait_ptr };
+    println!("Add: {}", test.add());
+    println!("Sub: {}", test.sub());
+    println!("Mul: {}", test.mul());
 
-    let masterfut = async {
-        let handle1 = tokio::spawn(fut1);
-        let handle2 = tokio::spawn(fut2);
-
-        handle1.await.unwrap();
-        handle2.await.unwrap();
-    };
-     
-    let mut rt = Runtime::new().unwrap();
-    rt.block_on(masterfut);
-    // println!("Tokio got: {}", val1);
-    // let val2 = rt.block_on(future2);
-    // println!("Tokio get: {}", val2);
-
-    reactor.lock().map(|mut r| r.close()).unwrap();
-    //block_on_all(futures, reactor);
+    // Or this can be simpler
+    // let test = unsafe { std::mem::transmute::<FatPointer, &dyn Test>(fat_pointer) };
+    // println!("ADD: {}", test.add());
 }
 
-//// ===== EXECUTOR =====
-fn block_on_all(mut futures: Vec<Task>, reactor: Arc<Mutex<Reactor>>) {
-    let waker = waker_new(thread::current());
-    loop {
-        let mut futures_to_remove = vec![];
-        for (i, future) in futures.iter_mut().enumerate() {
-            let pinned = Pin::new(future);
-            let waker = waker_into_waker(&waker);
-            let mut cx = Context::from_waker(&waker);
-            match Task::poll(pinned, &mut cx) {
-                Poll::Ready(val) => {
-                    println!("Got {}", val);
-                    futures_to_remove.push(i);
-                }
-                Poll::Pending => (),
-            };
-        }
-        for i in futures_to_remove {
-            futures.remove(i);
-        }
-        if futures.is_empty() {
-            let mut reactor_locked = reactor.lock().unwrap();
-            reactor_locked.close();
-            break;
-        }
+trait Test {
+    fn add(&self) -> i32;
+    fn sub(&self) -> i32;
+    fn mul(&self) -> i32;
+}
 
-        thread::park();
+#[repr(C)]
+struct FatPointer<'a> {
+    data: &'a mut Data,
+    vtable: *const usize,
+}
+
+impl<'a> FatPointer<'a> {
+    fn new(data: &'a mut Data, vtable: *const usize) -> Self {
+        FatPointer { data, vtable }
     }
 }
 
-// ===== FUTURE IMPLEMENTATION =====
-#[derive(Clone)]
-struct MyWaker {
-    thread: thread::Thread,
+struct Data {
+    a: i32,
+    b: i32,
 }
 
-#[derive(Debug)]
-enum Async<T: std::fmt::Debug> {
-    Pending,
-    Ready(T),
-}
-
-#[derive(Clone)]
-pub struct Task {
-    id: usize,
-    reactor: Arc<Mutex<Reactor>>,
-    data: u64,
-}
-
-fn waker_new(thread: thread::Thread) -> MyWaker {
-    MyWaker { thread }
-}
-
-fn waker_wake(s: &MyWaker) {
-    s.thread.unpark();
-}
-
-fn waker_clone(s: &MyWaker) -> RawWaker {
-    let s: *const MyWaker = s;
-    RawWaker::new(s as *const (), &VTABLE)
-}
-
-const VTABLE: RawWakerVTable = unsafe {
-    RawWakerVTable::new(
-        |s| waker_clone(&*(s as *const MyWaker)),
-        |s| waker_wake(&*(s as *const MyWaker)),
-        |_| {},
-        |_| {},
-    )
-};
-
-fn waker_into_waker(s: &MyWaker) -> Waker {
-    let self_data: *const MyWaker = s;
-    let raw_waker = RawWaker::new(self_data as *const (), &VTABLE);
-    unsafe { Waker::from_raw(raw_waker) }
-}
-
-impl Task {
-    fn new(reactor: Arc<Mutex<Reactor>>, data: u64) -> Self {
-        let id = reactor.lock().map(|mut r| r.generate_id()).unwrap();
-        Task { id, reactor, data }
-    }
-    
-}
-
-impl Future for Task {
-    type Output = usize;
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let mut r = self.reactor.lock().unwrap();
-        if r.is_ready(self.id) {
-            Poll::Ready(self.id)
-        } else {
-            r.register(self.data, cx.waker().clone(), self.id);
-            Poll::Pending
-        }
+impl Data {
+    fn new(a: i32, b: i32) -> Self {
+        Data { a, b }
     }
 }
 
-// ===== REACTOR =====
-struct Reactor {
-    dispatcher: Sender<Event>,
-    handle: Option<JoinHandle<()>>,
-    readylist: Arc<Mutex<Vec<usize>>>,
-    id_generator: usize,
+fn add(s: &Data) -> i32 {
+    s.a + s.b
 }
-
-enum Event {
-    Close,
-    Simple(Waker, u64, usize),
+fn sub(s: &Data) -> i32 {
+    s.a - s.b
 }
-
-impl Reactor {
-    fn new() -> Self {
-        let (tx, rx) = channel::<Event>();
-        let readylist = Arc::new(Mutex::new(vec![]));
-        let rl_clone = readylist.clone();
-        let mut handles = vec![];
-        let handle = thread::spawn(move || {
-            // This simulates some I/O resource
-            for event in rx {
-                let rl_clone = rl_clone.clone();
-                match event {
-                    Event::Close => break,
-                    Event::Simple(waker, duration, id) => {
-                        let event_handle = thread::spawn(move || {
-                            thread::sleep(Duration::from_secs(duration));
-                            rl_clone.lock().map(|mut rl| rl.push(id)).unwrap();
-                            waker.wake();
-                        });
-
-                        handles.push(event_handle);
-                    }
-                }
-            }
-
-            for handle in handles {
-                handle.join().unwrap();
-            }
-        });
-
-        Reactor {
-            readylist,
-            dispatcher: tx,
-            handle: Some(handle),
-            id_generator: 0,
-        }
-    }
-
-    fn register(&mut self, duration: u64, waker: Waker, data: usize) {
-        self.dispatcher
-            .send(Event::Simple(waker, duration, data))
-            .unwrap();
-    }
-
-    fn close(&mut self) {
-        self.dispatcher.send(Event::Close).unwrap();
-    }
-
-    fn is_ready(&self, id_to_check: usize) -> bool {
-        self.readylist
-            .lock()
-            .map(|rl| rl.iter().any(|id| *id == id_to_check))
-            .unwrap()
-    }
-
-    fn generate_id(&mut self) -> usize {
-        self.id_generator += 1;
-        self.id_generator
-    }
-}
-
-impl Drop for Reactor {
-    fn drop(&mut self) {
-        let handle = self.handle.take().unwrap();
-        handle.join().unwrap();
-    }
+fn mul(s: &Data) -> i32 {
+    s.a * s.b
 }
